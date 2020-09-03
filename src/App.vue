5d79a139f92b196450b39f9ad1ab07d12d25c876
C_yujie <1049250128@qq.com>
2020-09-03 14:05:54 +0800

    项目初始化-调整初始目录结构

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+<h1>黑马头条</h1>
+<!-- 路由出口 -->
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'App'
+}
+
+</script>
+<style lang="less">
+
+</style>
