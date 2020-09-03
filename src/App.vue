590facd804330ec86a00a6092fc1685cf116b82c
C_yujie <1049250128@qq.com>
2020-09-03 14:19:23 +0800

    项目初始化-引入vant组件库

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,28 @@
 <template>
   <div id="app">
-<h1>黑马头条</h1>
-<!-- 路由出口 -->
-    <router-view/>
+    <h1>黑马头条</h1>
+    <div>
+      <i class="toutiao toutiao-zuopin1"> </i>
+    </div>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+
+    <van-cell-group>
+      <van-cell title="单元格" value="内容" />
+      <van-cell title="单元格" value="内容" label="描述信息" />
+    </van-cell-group>
+    <!-- 路由出口 -->
+
+    <router-view />
   </div>
 </template>
 
 <script>
 export default {
-  name:'App'
+  name: 'App',
 }
-
 </script>
-<style lang="less">
-
-</style>
+<style lang="less"></style>
--- src/main.js
+++ src/main.js
@@ -3,13 +3,21 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 加载vant 核心组件库
+import Vant from 'vant'
+// 加载vant全局样式
+import 'vant/lib/index.css'
+
 //加载全局样式
-import  './styles/index.less'
+import './styles/index.less'
+
+// 注册使用vant 组件库
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount('#app')
